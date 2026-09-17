4cd2a0572a876c19c422e3375d2d4e6f6145cc8e
FirstSS <sota.monokuma.bass.1@gmail.com>
2026-09-17 12:17:42 +0900

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil && subRoot == nil {
+		return true
+	} else if root == nil || subRoot == nil {
+		return false
+	}
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot) || isSameTree(root, subRoot)
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+    if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
